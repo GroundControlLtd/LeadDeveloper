77fb8531a6f3ce766a39cc31b14f68e62b68f508
Anil Tailor <anil.tailor@mandg.com>
2021-10-13 15:50:06 +0100

    Add Tests for LibraryController and Garage Controller to ensure any changes to the code made do not break or change the expected output.

---
+++ Interview.Api.Tests/GarageControllerShould.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+using Xunit;
+
+namespace Interview.Api.Tests
+{
+    public class GarageControllerShould
+    {
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        [InlineData("Unknown", "Unknown")]
+        [InlineData("Nissan", "Primera")]
+        [InlineData("Lotus", "Espirit")]
+        public void ReturnExpectedDateWhenBookingMotGivenACar(string carMake, string carModel)
+        {
+            //Arrange
+            var controller = new GarageController();
+            var car = new Car { Make=carMake,Model=carModel };
+            var date = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 11, 15, 0).AddDays(3);
+
+            if (date.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday)
+            {
+                date = date.AddDays(2);
+            }
+            var expectedValue = $"{car.Make} {car.Model} MOT booked for {date} ";
+
+            //Act
+            var response = controller.BookMot(car);
+            var actualValue = ((ObjectResult)response).Value;
+            //Assert
+            Assert.NotNull(actualValue);
+            Assert.Equal(expectedValue, actualValue);
+        }
+    }
+}
---
+++ Interview.Api.Tests/LibraryControllerShould.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+using Xunit;
+
+namespace Interview.Api.Tests
+{
+    public class LibraryControllerShould
+    {
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        [InlineData("Unknown", "Unknown")]
+        [InlineData("Brave New World", "Unknown")]
+        [InlineData("Unknown", "Aldous Huxley")]
+        public void ReturnNotFoundWhenBorrowingBookGivenUnknownBookTitleOrBookAuthor(string bookTitle, string bookAuthor)
+        {
+            //Arrange
+            const string expectedValue = "Book not found!";
+            var controller = new LibraryController();
+            var book = new Book {Title=bookTitle, Author = bookAuthor};
+            //Act
+            var response = controller.BorrowBook(book);
+            var actualValue = ((ObjectResult)response).Value;
+            //Assert
+            Assert.NotNull(actualValue);
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Theory]
+        [InlineData("Brave New World", "Aldous Huxley")]
+        public void ReturnExpectedValueWhenBorrowingBookGivenKnownBookAndBookAuthor(string bookTitle, string bookAuthor)
+        {
+            //Arrange
+            var returnDate = DateTime.Now.AddDays(30).ToString("dd-MMM-yyyy");
+            var expectedValue = $"{bookTitle} borrowed until the {returnDate}";
+            var controller = new LibraryController();
+            var book = new Book { Title = bookTitle, Author = bookAuthor };
+            //Act
+            var response = controller.BorrowBook(book);
+            var actualValue = ((ObjectResult)response).Value;
+            //Assert
+            Assert.NotNull(actualValue);
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+    }
+}
